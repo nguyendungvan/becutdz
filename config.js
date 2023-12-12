e2e901bda23e5b0ba6a9b053abe8900a6ca57f4d
nguyendungvan <108008068+nguyendungvan@users.noreply.github.com>
2023-12-12 11:16:18 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,49 @@
+module.exports = {
+	helpCmdPerPage: 10, //- Number of commands per page of help command
+	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
+	adminId: "457074467319840768", //- Replace UserId with the Discord ID of the admin of the bot
+	token: process.env.token || "MTE4MTkyNDEwNjY4NjQzNTMzOA.GdGgUd.n538R1VGEhW9B4tbpuDZYMUlh5xG_-poXwbZZo", //- Bot's Token
+	clientId: process.env.clientId || "1181924106686435338", //- ID of the bot
+	clientSecret: process.env.clientSecret || "xeXL77vA-CrwT5so_GBtIvkwrBj61t4y", //- Client Secret of the bot
+	port: 4200, //- Port of the API and Dashboard
+	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
+	inviteScopes: ["bot", "applications.commands"], // Invite link scopes
+	serverDeafen: true, //- If you want bot to stay deafened
+	defaultVolume: 100, //- Sets the default volume of the bot, You can change this number anywhere from 1 to 100
+	supportServer: "<@277548067115499520>", //- Support Server Link
+	Issues: "<@277548067115499520>", //- Bug Report Link
+	permissions: 277083450689, //- Bot Inviting Permissions
+	disconnectTime: 30000, //- How long should the bot wait before disconnecting from the voice channel (in miliseconds). Set to 1 for instant disconnect.
+	twentyFourSeven: false, //- When set to true, the bot will never disconnect from the voice channel
+	autoQueue: false, //- When set to true, related songs will automatically be added to the queue
+	autoPause: true, //- When set to true, music will automatically be paused if everyone leaves the voice channel
+	autoLeave: false, //- When set to true, the bot will automatically leave when no one is in the voice channel (can be combined with 24/7 to always be in voice channel until everyone leaves; if 24/7 is on disconnectTime will add a disconnect delay after everyone leaves.)
+	debug: false, //- Debug mode
+	cookieSecret: "Cutdzvclhihi", //- Cookie Secret
+	website: "http://localhost:4200", //- without the / at the end
+	// You need a lavalink server for this bot to work!!!!
+	// Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
+	nodes: [
+		{
+			identifier: "Main Node", //- Used for indentifier in stats commands.
+			host: "lavalink.lexnet.cc", //- The host name or IP of the lavalink server.
+			port: 443, // The port that lavalink is listening to. This must be a number!
+			password: "lexn3tl@val!nk", //- The password of the lavalink server.
+			retryAmount: 2000, //- The amount of times to retry connecting to the node if connection got dropped.
+			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
+			secure: true, //- Can be either true or false. Only use true if ssl is enabled!
+		},
+	],
+	embedColor: "#2f3136", //- Color of the embeds, hex supported
+	presence: {
+		// PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
+		status: "online", //- You can have online, idle, dnd and invisible (Note: invisible makes people think the bot is offline)
+		activities: [
+			{
+				name: "cùng tớ nhaaa", //- Status Text
+				type: "LISTENING", //- PLAYING, WATCHING, LISTENING, STREAMING
+			},
+		],
+	},
+	iconURL: "https://cdn.darrennathanael.com/icons/spinning_disk.gif", //- This icon will be in every embed's author field
+};
